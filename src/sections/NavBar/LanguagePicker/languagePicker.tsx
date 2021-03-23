65ae01232c7e08d9dce60f39aac7c94da7f95c8d
Sneyder <sneyder2328@gmail.com>
2021-03-23 14:19:09 -0400

    Make all sections have the same margin

--- src/sections/NavBar/LanguagePicker/languagePicker.tsx
+++ src/sections/NavBar/LanguagePicker/languagePicker.tsx
@@ -7,6 +7,6 @@ export const LanguagePicker = ({langKey}) => {
     return (<div className={styles.languagePicker}>
         <Link to={"/"} className={classNames(styles.langLabel, {[styles.active]: langKey === "en"})}>EN</Link>
         <span className={styles.separator}>/</span>
-        <Link to={"/es"} className={classNames(styles.langLabel, {[styles.active]: langKey === "es"})}>ES</Link>
+        <Link to={"/es/"} className={classNames(styles.langLabel, {[styles.active]: langKey === "es"})}>ES</Link>
     </div>)
 }
