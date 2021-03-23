76b3f58f0319f345d1f0b929df8368fa6dc22832
Sneyder <sneyder2328@gmail.com>
2021-03-23 10:54:30 -0400

    Change 404 page for / redirect

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,9 @@
 import * as React from "react"
+import { navigate } from 'gatsby'
 
-const NotFoundPage = () => (
-  <div>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
-)
-
-export default NotFoundPage
+export default () => {
+  React.useEffect(() => {
+    navigate('/');
+  }, []);
+  return null;
+};
